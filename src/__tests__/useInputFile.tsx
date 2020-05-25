a967a1564bae973f9cdc1c6e2f25f155023cd9fb
Peng Jie <bivinity.pengzjie@gmail.com>
2020-05-25 09:29:54 +0800

    test: add useInputFile tests

---
+++ src/__tests__/useInputFile.tsx
@@ -0,0 +1,83 @@
+import React, { FC } from 'react';
+import { act, renderHook } from '@testing-library/react-hooks';
+import { fireEvent, render } from '@testing-library/react';
+import useInputFile, { Options } from '..';
+
+const setup = () => {
+  const ref = { current: document.createElement('input') };
+  const file = new File([`(¯\_(ツ)_/¯)`], 'me.png', {
+    type: 'image/png'
+  });
+
+  return { ref, file };
+};
+
+describe('useInputFile', () => {
+  describe('ref', () => {
+    it('should set input type as file ', () => {
+      const Component: FC = () => {
+        const { ref } = useInputFile();
+
+        return <input ref={ref} />;
+      };
+      const { container } = render(<Component />);
+
+      expect(container.firstChild).toHaveAttribute('type', 'file');
+    });
+
+    it('should set input type as file with custom ref', () => {
+      const { ref } = setup();
+      const { result } = renderHook(() => useInputFile({ ref }));
+      const input = result.current.ref.current as HTMLInputElement;
+
+      expect(input.type).toBe('file');
+    });
+  });
+
+  describe('options', () => {
+    it('should set input `accept` attributes', () => {
+      const { ref } = setup();
+      const options: Options = { accept: 'image/*' };
+      const { result } = renderHook(() => useInputFile({ ref, options }));
+      const input = result.current.ref.current as HTMLInputElement;
+
+      expect(input).toHaveAttribute('accept');
+      expect(input.getAttribute('accept')).toBe('image/*');
+    });
+
+    it('should set input `multiple` attributes', () => {
+      const { ref } = setup();
+      const options: Options = { multiple: true };
+      const { result } = renderHook(() => useInputFile({ ref, options }));
+      const input = result.current.ref.current as HTMLInputElement;
+
+      expect(input).toHaveAttribute('multiple');
+      expect(input.getAttribute('multiple')).toBe('multiple');
+    });
+  });
+
+  describe('files', () => {
+    it('should called custom onChange once', () => {
+      const { ref, file } = setup();
+      const onChange = jest.fn();
+      renderHook(() => useInputFile({ ref, onChange }));
+
+      act(() => {
+        fireEvent.change(ref.current, { target: { files: [file] } });
+      });
+
+      expect(onChange).toBeCalled();
+    });
+
+    it('should get a file by change input file', () => {
+      const { ref, file } = setup();
+      const { result } = renderHook(() => useInputFile({ ref }));
+
+      act(() => {
+        fireEvent.change(ref.current, { target: { files: [file] } });
+      });
+
+      expect(result.current.files).toEqual([file]);
+    });
+  });
+});
